bb47c0a8aee0bbac4d8016a9e61951f23ad0a4da
ito-ito <ittooco@gmail.com>
2024-11-14 23:49:12 +0900

    :recycle: Headerをコンポーネントとして切り出し

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/cloudflare";
+import HeaderContainer from "app/components/header";
 
 export const meta: MetaFunction = () => {
   return [{ title: "Snow School Shift" }, { name: "description", content: "Welcome to Remix!" }];
@@ -7,19 +8,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <>
-      <div className="w-full py-8 sm:py-12 bg-zinc-100">
-        <div className="container mx-auto px-4">
-          <h1 className="text-3xl font-semibold tracking-tight text-zinc-900 sm:text-6xl">
-            スクール勤務予定表
-          </h1>
-          <p className="mt-3 text-xl font-semibold bg-gradient-to-r from-cyan-500 to-blue-500 inline-block text-transparent bg-clip-text sm:text-4xl">
-            スノーボード
-          </p>
-          <p className="mt-4 text-pretty text-sm font-medium text-zinc-600 sm:text-xl">
-            シフトスケジュール予定です
-          </p>
-        </div>
-      </div>
+      <HeaderContainer />
       <main>
         <div className="container mx-auto px-4"></div>
       </main>
