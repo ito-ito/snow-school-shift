4dd024cd82d5fa4eec8eb47675d11c501bdedf82
ito-ito <ittooco@gmail.com>
2024-11-14 22:33:35 +0900

    :package: Install shadcn-ui

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,15 +7,15 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <>
-      <div className="w-full py-8 sm:py-12 bg-slate-100 dark:bg-slate-900">
+      <div className="w-full py-8 sm:py-12 bg-zinc-100">
         <div className="container mx-auto px-4">
-          <h1 className="text-3xl font-semibold tracking-tight text-slate-900 dark:text-slate-100 sm:text-6xl">
+          <h1 className="text-3xl font-semibold tracking-tight text-zinc-900 sm:text-6xl">
             スクール勤務予定表
           </h1>
           <p className="mt-3 text-xl font-semibold bg-gradient-to-r from-cyan-500 to-blue-500 inline-block text-transparent bg-clip-text sm:text-4xl">
             スノーボード
           </p>
-          <p className="mt-4 text-pretty text-sm font-medium text-slate-600 dark:text-slate-400 sm:text-xl">
+          <p className="mt-4 text-pretty text-sm font-medium text-zinc-600 sm:text-xl">
             シフトスケジュール予定です
           </p>
         </div>
