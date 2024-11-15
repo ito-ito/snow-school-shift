ef3f6226dd72601eeaa2fd2a082c6a0fae8a8edb
ito-ito <ittooco@gmail.com>
2024-11-15 22:09:10 +0900

    :lipstick: タブを追加

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from "@remix-run/cloudflare";
 import HeaderContainer from "app/components/header";
+import MonthTabContainer from "~/components/monthTab";
 
 export const meta: MetaFunction = () => {
   return [{ title: "Snow School Shift" }, { name: "description", content: "Welcome to Remix!" }];
@@ -10,7 +11,9 @@ export default function Index() {
     <>
       <HeaderContainer />
       <main>
-        <div className="container mx-auto px-4"></div>
+        <div className="container mx-auto px-4 mt-4">
+          <MonthTabContainer />
+        </div>
       </main>
     </>
   );
